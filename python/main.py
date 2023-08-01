2beb1afca6266ae27918622cf0c2ca7a4d69b967
1140982 <ryan.e.mason@raytheon.com>
2023-07-31 20:21:39 -0700

    started python

---
+++ python/main.py
@@ -0,0 +1,24 @@
+
+
+def main():
+    test_is_unique()
+
+def test_is_unique():
+    print("is_unique result (avbbc): ",is_unique("avbbc"))
+    print("is_unique result (abcde): ",is_unique("abc"))
+
+# Implement an algorithm to determine if a string has all unique characters
+def is_unique(input: str) -> bool:
+    char_dict = {}
+
+    for c in input:
+        if char_dict.__contains__(c):
+            return False
+        else:
+            char_dict[c] = 1
+            # current_value = char_dict.get(c)
+            # char_dict.update(c, current_value+1)
+    return True
+
+if __name__ == "__main__":
+    main()
